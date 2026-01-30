e4efe7edac3d1f54245f7ec06f7effb6e5ddd215
lsabri <130772394+lsabri@users.noreply.github.com>
2026-01-30 18:15:54 +0100

    qd

--- debugg.js
+++ debugg.js
@@ -6,8 +6,9 @@ dotenv.config();
 const NINJA_URL = process.env.NINJA_URL;
 const CLIENT_ID_NINJA = process.env.CLIENT_ID_NINJA;
 const AUTH_SECRET_NINJA = process.env.AUTH_SECRET_NINJA;
-
+const CREDENTIALS_PATH = process.env.CREDENTIALS_PATH;
 
 console.log(`🔍 URL ${NINJA_URL} `);
 console.log(`🔍 CAUTH_SECRET_NINJA ${AUTH_SECRET_NINJA}`);
+console.log(`🔍CREDENTIALS_PATH ${CREDENTIALS_PATH}`);
 //////
